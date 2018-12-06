4afb4857f2ef6e193a2b172410bb19d2ea5b1706
Chang Sophia <you@example.com>
2018-12-06 14:36:05 +0800

    Initial Commit

---
+++ MamaMia!/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MamaMia!
+//
+//  Created by Chang Sophia on 12/6/18.
+//  Copyright © 2018 Chang Sophia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
